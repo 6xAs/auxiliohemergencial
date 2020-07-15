bb2bc03be8399c641cfeae8275934771d650140e
Anderson Seixas <andersonseixas.xt@gmail.com>
2020-07-15 11:52:41 -0300

    fase inicial bd ok, model ok

---
+++ database/migrations/2020_07_15_142925_create_brasileiros_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrasileirosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brasileiros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->nullable();
+            $table->string('cpf')->nullable();
+            $table->date('data_nascimento')->nullable();
+            $table->string('nome_mae')->nullable();
+            $table->string('celular')->nullable();
+            $table->decimal('quanto_ganhava')->nullable();
+            $table->string('enderco')->nullable();
+            $table->string('cidade')->nullable()
+            $table->string('estado')->nullable();
+            $table->string('banco')->nullable();
+            $table->string('agencia')->nullable();
+            $table->string('conta_corrente')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('update_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brasileiros');
+    }
+}
